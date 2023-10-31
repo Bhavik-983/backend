11781965e9bd22159702917ba9c04bc055b5f51a
Bhavik <bhavik.softcolon@gmail.com>
2023-10-31 17:01:28 +0530

    fix change

--- App.js
+++ App.js
@@ -59,7 +59,7 @@ app.use("/api", stripeRoutes);
 //   })
 // });
 // Server response port
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on\nhttp://localhost:${port}`);
 });
